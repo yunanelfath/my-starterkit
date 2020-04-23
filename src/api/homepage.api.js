6d8ded9acd747e7ade18a2919317efd980b64196
Yunankf <yunanelfath@gmail.com>
2020-04-23 10:10:30 +0700

    fix(homepage): did mount get api

---
+++ src/api/homepage.api.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter"
+
+
+export const homepageApi = {
+  root: "/home",
+};
+
+export function apiGetHomepage(){
+  let baseUri = 'https://private-4639ce-ecommerce56.apiary-mock.com'
+  let url = homepageApi.root
+
+  const customAxios = axios.create({
+    baseURL: baseUri,
+  })
+
+  return new Promise((resolve, reject)=>{
+
+    if(url.match(/fake/g)){
+
+      let mock = new MockAdapter(axios, { delayResponse: 1000 });
+        mock.onPost(url).replyOnce(200, {
+          data: {
+            result: true,
+          }
+        });
+
+        axios
+          .get(url )
+          .then(response => {
+            mock.restore();
+            resolve({response: response.data})
+          })
+          .catch(err => {
+            return { error: err.response };
+          });
+      }else{
+
+        customAxios
+        .get(url)
+        .then(response => {
+          resolve({ response: response.data })
+        })
+        .catch(err => {
+          // let err =  { error: err.response };
+          reject(err.response)
+        });
+
+
+      }
+
+  })
+
+}
--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import BottomNavigation from 'components/BottomNavigation'
+import {
+  apiGetHomepage
+} from 'api/homepage.api'
 
-function App() {
-  return (
-    <>
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        ini homepage
-      </header>
-    </div>
-    <BottomNavigation />
-    </>
-  );
+class Homepage extends React.Component {
+  // constructor(props){
+  //   super(props)
+  // }
+  async componentDidMount(){
+    await apiGetHomepage().then((e)=>{
+      const {
+        response
+      } = e
+      console.log(response[0]);
+    })
+  }
+  render(){
+    return (
+      <>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          ini homepage
+        </header>
+      </div>
+      <BottomNavigation />
+      </>
+    );
+  }
 }
-
-export default App;
+export default Homepage;
