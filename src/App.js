c30bba202087cacccacd852e098ed0da1f2f5b67
Yunankf <yunanelfath@gmail.com>
2020-05-06 11:57:06 +0700

    fix(scss): zlib error after install on windows
    npm cache clean --force --verbose && npm shrinkwrap && npm install https://npm.community/t/npm-err-code-z-buf-error-npm-err-errno-5-npm-err-zlib-unexpected-end-of-file/3303

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import style from 'scss/homepage/style.scss'
 
 import { Provider } from 'react-redux'
 import configureStore from 'store/Store';
@@ -8,7 +9,7 @@ import Route from 'router/Routes'
 // import Loading from 'components/Loading'
 
 const { persistor, store } = configureStore();
-
+console.log(style);
 function App() {
   return (
     <Provider store={store}>
